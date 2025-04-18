bd3040dbb2aed1534e712d362e312023282d1588
Маков Сергей Константинович <s.makov@Ampere.local>
2025-04-18 17:27:06 +0300

    Initial commit

---
+++ PCAN/RunExample.cpp
@@ -0,0 +1,13 @@
+#include "03_ManualRead.h"
+#include "04_ManualWrite.h"
+
+int main()
+{
+    bool isRead = true;
+    if (isRead) {
+        ManualRead start;
+    }
+    else {
+        ManualWrite start;
+    }
+}
